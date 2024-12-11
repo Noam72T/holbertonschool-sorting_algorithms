a5c73a71714b9586dd768d7b2e51b2bc99976566
Noam72T <9823@holbertonstudents.com>
2024-12-11 14:18:02 +0100

    C - Sorting algorithms & Big O Exercie 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,22 +6,26 @@
  * @size: Size of the array.
  */
 
-void bubble_sort(int *array, size_t size) 
+void bubble_sort(int *array, size_t size)
 {
-  if (array == NULL || size < 2)
-    return;
+    size_t i, j;
+    int temp;
 
-  for (size_t i = 0; i < size - 1; i++) {
-    for (size_t j = 0; j < size - i - 1; j++) 
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
     {
-      if (array[j] > array[j + 1]) {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
 
-        int temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-
-        print_array(array, size);
-      }
+                print_array(array, size);
+            }
+        }
     }
-  }
 }
