e9a49c9c761ddb8daf2a85018a04030604a3e348
JorreJ <jade.jorre@gmail.com>
2024-12-13 09:55:52 +0100

    fix betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,32 +7,35 @@
  * @list: Double pointer.
  */
 
-void insertion_sort_list(listint_t **list) {
-  listint_t *current, *temp;
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
 
-  if (list == NULL || *list == NULL || (*list)->next == NULL)
-    return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-  current = (*list)->next;
-  while (current != NULL) {
-    temp = current;
-    while (temp->prev != NULL && temp->n < temp->prev->n) {
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
 
-      temp->prev->next = temp->next;
-      if (temp->next != NULL)
-        temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
 
-      temp->next = temp->prev;
-      temp->prev = temp->prev->prev;
-      temp->next->prev = temp;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-      if (temp->prev != NULL)
-        temp->prev->next = temp;
-      else
-        *list = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
 
-      print_list(*list);
-    }
-    current = current->next;
-  }
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
